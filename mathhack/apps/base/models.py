02e4d8bda4aa235545bc8db996ceaecf139707f0
Brian Fife <Fife@BN-BFife-Mac.local>
2013-04-28 15:06:13 -0400

    Added initial draft of schema.

--- mathhack/apps/base/models.py
+++ mathhack/apps/base/models.py
@@ -2,4 +2,30 @@ from sqlalchemy import Column, Integer
 
 from mathhack.database.classes import Model
 
+class Problem(Model):
+    __tablename__ = 'problem'
+    
+    id = Column(Integer, Sequence('problem_id_seq'), primary_key=True,
+                autoincrement=True)
+    numerator = Column(Integer())
+    denominator = Column(Integer())
+    operation = Column(String())
+    answer = Column(Integer())
 
+class User(Model): 
+    __tablename__ = 'user'
+    
+    id = Column(Integer, Sequence('user_id_seq'), primary_key=True,
+                autoincrement=True)
+    email = Column(String())
+    name = Column(String())
+
+
+class Answer(Model):
+    __tablename__ = 'answer'
+    
+    problem_id = Column(Integer, ForeignKey("problem.id"))
+    user_id = Column(Integer, ForeignKey("user.id"))
+    timestamp = Column(Integer())
+    answer = Column(Integer())
+    delay = Column(Integer())
